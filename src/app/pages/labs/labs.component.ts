439482a33197324f209c088f70ef7ae475df217c
JairLauterio <jairlauterio14@gmail.com>
2024-05-27 17:37:12 -0700

    Primer Version Front-end

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 @Component({
-  selector: 'app-root',
+  selector: 'app-labs',
   standalone: true,
   imports: [RouterOutlet, CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
 })
-export class AppComponent {
+export class LabsComponent {
   title = 'SegurosSonora';
   welcome = 'Hola';
   tasks = [
