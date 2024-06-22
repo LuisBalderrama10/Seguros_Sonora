6b2862f5c1b5da05507c35165b94c25c48b96c3d
LuisBalderrama10 <lbalderrama2001@gmail.com>
2024-06-21 23:08:19 -0700

    Se agrego conexion a la base de datos

--- api/seguros_api/dist/app.module.js
+++ api/seguros_api/dist/app.module.js
@@ -8,6 +8,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
+const TypeOrm_1 = require("@nestjs/TypeOrm");
 const vehiculos_controller_1 = require("./vehiculo/vehiculos.controller");
 const vehiculos_services_1 = require("./vehiculo/vehiculos.services");
 const aseguradora_controllers_1 = require("./aseguradora/aseguradora.controllers");
@@ -20,6 +21,21 @@ exports.AppModule = AppModule = __decorate([
         imports: [],
         controllers: [vehiculos_controller_1.VehiculoController, aseguradora_controllers_1.AseguradoraController],
         providers: [vehiculos_services_1.VehiculoServicio, aseguradoras_services_1.AseguradoraServicio],
+    }),
+    (0, common_1.Module)({
+        imports: [
+            TypeOrm_1.TypeOrmModule.forRoot({
+                type: 'postgres',
+                host: '192.168.90.209',
+                port: 5432,
+                username: 'seguros_inst_db',
+                password: '1920siadmin',
+                database: 'sonora_dig',
+                entities: [__dirname + '/**/*.entity{.ts,.js}'],
+                synchronize: true,
+                logging: 'all',
+            }),
+        ],
     })
 ], AppModule);
 //# sourceMappingURL=app.module.js.map
--- api/seguros_api/src/app.module.ts
+++ api/seguros_api/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/TypeOrm';
 import { VehiculoController } from './vehiculo/vehiculos.controller';
 import { VehiculoServicio } from './vehiculo/vehiculos.services';
 import { AseguradoraController } from './aseguradora/aseguradora.controllers';
@@ -8,5 +9,21 @@ import { AseguradoraServicio } from './aseguradora/aseguradoras.services';
   imports: [],
   controllers: [VehiculoController, AseguradoraController],
   providers: [VehiculoServicio, AseguradoraServicio],
+})
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '192.168.90.209',
+      port: 5432,
+      username: 'seguros_inst_db',
+      password: '1920siadmin',
+      database: 'sonora_dig',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logging: 'all', // Habilitar logging
+    }),
+  ],
 })
 export class AppModule {}
