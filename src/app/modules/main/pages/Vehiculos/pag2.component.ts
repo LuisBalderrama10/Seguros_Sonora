a25f0d21e34cf646376a962f480e8940690cb69e
LuisBalderrama10 <lbalderrama2001@gmail.com>
2024-09-26 02:45:24 -0700

    Co-authored-by: CalebBeltran <CalebBeltran@users.noreply.github.com> Co-authored-by: rosariovelasquez <rosariovelasquez@users.noreply.github.com>

--- src/app/modules/main/pages/Vehiculos/pag2.component.ts
+++ src/app/modules/main/pages/Vehiculos/pag2.component.ts
@@ -1,29 +1,21 @@
-import { HttpClient } from '@angular/common/http';
-
-import { Component, HostListener, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { SidebarComponent } from '../../components/sidebar/sidebar.component'
-import { BuscadorComponent } from '../../components/buscador/buscador.component'
-import { ApiService } from '../../../../../../services/api.service';
+import { Component, OnInit } from '@angular/core';
+import { TuTablaService } from '../../services/api.service';
 
 @Component({
   selector: 'app-pag2',
-  standalone: true,
-  imports: [RouterOutlet, CommonModule, SidebarComponent, BuscadorComponent],
   templateUrl: './pag2.component.html',
-  styleUrl: './pag2.component.css'
+  styleUrls: ['./pag2.component.css']
 })
-
 export class Pag2Component implements OnInit {
-  items: any[] = [];
+  datos: any[] = [];
 
-  constructor(private apiService: ApiService) { }
+  constructor(private tuTablaService: TuTablaService) {}
 
   ngOnInit(): void {
-    this.apiService.getItems().subscribe((data: any[]) => {
-      this.items = data;
+    this.tuTablaService.getDatos().subscribe((data) => {
+      this.datos = data;
     });
+  }
 }
 
-}
+
